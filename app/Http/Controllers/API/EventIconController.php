0e67ec114c4fd2d521a48d6a976364cb416aff71
panda1114Git <panda.kk1114@gmail.com>
2022-01-13 21:25:51 +0200

    uploading validation

--- app/Http/Controllers/API/EventIconController.php
+++ app/Http/Controllers/API/EventIconController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\EventIconRequest;
 use App\Models\EventIcon;
 
 class EventIconController extends Controller
 {
 
-    public function index(Request $request )
+    public function index(EventIconRequest $request )
     {
         $auth_id = auth()->user()->id;
         $event_icons = EventIcon::where("user_id", $auth_id)->orWhere("user_id", null )->get();
@@ -22,24 +22,28 @@ class EventIconController extends Controller
         return response( [] );
     }
 
-    public function store(Request $request )
+    public function store(EventIconRequest $request )
     {
         $auth_id = auth()->user()->id;
-        $files = $request->file('icon');
 
-            foreach ($files as $file) {
-                $name = $file->getClientOriginalName();
-                $file->move(public_path() . '/uploads/icons/', $name);
+        if($request->hasFile('icon')){
 
-                $icon = new EventIcon([
-                    "user_id" => $auth_id,
-                    "path" => '/uploads/icons/'.$file->getClientOriginalName()
-                ]);
-                $icon->save(); // Finally, save the record.
+            $files = $request->file('icon');
+
+                foreach ($files as $file) {
+                    $name = $file->getClientOriginalName();
+                    $file->move(public_path() . '/uploads/icons/', $name);
+
+                    $icon = new EventIcon([
+                        "user_id" => $auth_id,
+                        "path" => '/uploads/icons/'.$file->getClientOriginalName()
+                    ]);
+                    $icon->save(); // Finally, save the record.
 
 
-            }
+                }
 
-        return response([ 'event' => $request ]);
+            return response([ 'event' => $request ]);
+        }
     }
 }
