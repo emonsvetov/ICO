8b096c9c6f6dac1ee55b2f90a936385eaa875dd3
panda1114Git <panda.kk1114@gmail.com>
2022-01-14 13:10:10 +0200

    upload validation updated

--- app/Http/Controllers/API/EventIconController.php
+++ app/Http/Controllers/API/EventIconController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\EventIconRequest;
 use App\Models\EventIcon;
 use Illuminate\Support\Facades\Validator;
 
@@ -13,8 +12,8 @@ class EventIconController extends Controller
 
     public function index(Request $request )
     {
-        $auth_id = 1;
-        // $auth_id = auth()->user()->id;
+        // $auth_id = 1;
+        $auth_id = auth()->user()->id;
         $event_icons = EventIcon::where("user_id", $auth_id)->orWhere("user_id", null )->get();
         if ( $event_icons->isNotEmpty() )
         {
@@ -26,13 +25,13 @@ class EventIconController extends Controller
 
     public function store(Request $request )
     {
-        // $auth_id = auth()->user()->id;
-        $auth_id = 1;
+        $auth_id = auth()->user()->id;
+        // $auth_id = 1;
 
         $validator = Validator::make($request->all(),
         [
             'icon' => 'required',
-            'icon.*' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'icon.*' => 'required|image|mimes:jpeg,png,jpg,gif,ico|max:2048'
         ]
         );
         if($validator->fails()) {
@@ -40,12 +39,14 @@ class EventIconController extends Controller
         }
         if($request->has('icon')) {
             foreach($request->file('icon') as $image) {
-                $filename = time().rand(3). '.'.$image->getClientOriginalExtension();
+                // $filename = time().rand(3, 5). '.'.$image->getClientOriginalExtension();
+
+                $filename = '/uploads/icons/'.$image->getClientOriginalName();
                 $image->move(public_path() . '/uploads/icons/', $filename);
 
                 EventIcon::create([
                     "user_id" => $auth_id,
-                    "path" => '/uploads/icons/'.$filename
+                    "path" => $filename
                 ]);
             }
 
@@ -59,11 +60,6 @@ class EventIconController extends Controller
             $response["message"] = "Failed! image(s) not uploaded";
         }
         return response()->json($response);
-
-        // $icon = new EventIcon([
-        //     "user_id" => $auth_id,
-        //     "path" => '/uploads/icons/'.$file->getClientOriginalName()
-        // ]);
 
     }
 }
