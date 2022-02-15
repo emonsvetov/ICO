b37103b92ec6c76574930eeca777ee0e6b9f903a
Arvind Kumar <arvind@inimisttech.com>
2022-02-15 21:28:19 +0530

    updating merchant model

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Merchant extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $guarded = [];
+
+    public function children()
+    {
+        return $this->hasMany(Merchant::class, 'parent_id')->with('children');
+    }
 }
