e8df59775ee2d6dcf51b3b740c5e11c3cc2484e3
Arvind Kumar <arvind@inimisttech.com>
2022-05-20 18:27:33 +0530

    updating migration

--- database/migrations/2022_05_07_054700_update_primary_index_in_model_has_roles_table.php
+++ database/migrations/2022_05_07_054700_update_primary_index_in_model_has_roles_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\PermissionRegistrar;
+use DB;
 
 class UpdatePrimaryIndexInModelHasRolesTable extends Migration
 {
@@ -17,9 +18,11 @@ class UpdatePrimaryIndexInModelHasRolesTable extends Migration
         Schema::disableForeignKeyConstraints();
         $columnNames = config('permission.column_names');
         Schema::table('model_has_roles', function (Blueprint $table) use ($columnNames) {
+            $table->dropForeign('model_has_roles_role_id_foreign');
             $table->dropPrimary([PermissionRegistrar::$pivotRole, $columnNames['model_morph_key'], 'model_type']);
             // DB::unprepared('ALTER TABLE `model_has_roles` DROP PRIMARY KEY;');
             $table->primary([PermissionRegistrar::$pivotRole, $columnNames['model_morph_key'], 'model_type', 'program_id'], 'model_has_roles_role_model_type_program_primary');
+            DB::statement('ALTER TABLE `model_has_roles` ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE');
         });
         Schema::enableForeignKeyConstraints();
     }
