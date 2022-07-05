0d4392751bac27268d5afce16dd94756d2ed464d
Amol Kaur <amol@inimisttech.com>
2022-07-05 19:22:38 +0530

    Invite participant

--- app/Http/Controllers/API/InvitationController.php
+++ app/Http/Controllers/API/InvitationController.php
@@ -7,34 +7,34 @@ use App\Http\Controllers\Controller;
 use App\Models\Organization;
 use App\Models\Program;
 use App\Models\User;
+use App\Models\Role;
 use DB;
 
 class InvitationController extends Controller
 {
-
+    /**
+     * Participant Invitation 
+     */
     public function invite(InvitationRequest $request, Organization $organization, Program $program)
     {
-		//file_put_contents("test.txt",json_encode($request->all()));
-     //return auth()->user();
+        //return auth()->user();
 		try {
             $validated = $request->validated();
             $validated['organization_id'] = $organization->id;
-            $validated['password']="123456";
+            $validated['password']=rand();
             $user = User::createAccount( $validated );
-			//$validated['roles'] get prpgram here
-			//file_put_contents("test.txt",json_encode($user));
-          //  if( !empty($validated['roles']))   {
-               // $user->syncRoles( [$validated['roles']] );
-			 //   $user->syncProgramRoles($program->id, $validated['roles']); //here pass program id
-           // }
-		   // $program_id = $validated['program_id'];
             $program_id=$program->id;
-			$columns = []; //any additional columns set here
-			//$user->programs()->sync( [ $validated['program_id'] => $columns ], false); need to discuss this
+            //file_put_contents("test.txt",json_encode($user));
+            //if( !empty($validated['roles']))   {
+            //$user->syncRoles( [$validated['roles']] );
+            //$user->syncProgramRoles($program->id, $validated['roles']); //here pass program id
+            //}
+            //$program_id = $validated['program_id'];
+            //$columns = []; //any additional columns set here
+            //$user->programs()->sync( [ $validated['program_id'] => $columns ], false); need to discuss this
 
-          //  $roles = $validated['roles'];
-          $roles=[6];
-
+            //$roles = $validated['roles'];
+            $roles[] = Role::getIdByName(config('roles.participant'));
             if( !empty($roles) ) {
                 $user->syncProgramRoles($program_id, $roles);
             }
@@ -42,6 +42,5 @@ class InvitationController extends Controller
         } catch (\Exception $e )    {
             return response(['errors' => $e->getMessage()], 422);
         }
-		//"This person is already a participant
 	}
 }
