3c179e84999dec1deb6bca01fa49bde978305169
Amol Kaur <amol@inimisttech.com>
2022-08-04 20:54:12 +0530

    goal plan validations

--- app/Http/Requests/GoalPlanRequest.php
+++ app/Http/Requests/GoalPlanRequest.php
@@ -40,19 +40,19 @@ class GoalPlanRequest extends FormRequest
             'achieved_callback_id'=>'string',
             'exceeded_callback_id'=>'string',
             'achieved_event_id '=>'sometimes|integer',
-            'exceeded_event_id '=>'sometimes|integer',
+            'exceeded_event_id '=>'required_if:goal_plan_type_id,1|integer', //required if sales goal (id-1)
             'automatic_progress'=> 'required|boolean',
-            'automatic_frequency'=>'sometimes|string',
-            'automatic_value'=>'sometimes|integer',
+            'automatic_frequency'=>'required_if:automatic_progress,1|string',
+            'automatic_value'=>'required_if:automatic_progress,1|integer',
             'expiration_rule_id'=>'required|integer',
-            'custom_expire_offset'=>'sometimes|integer', //if expiration_rule_id is custom
-            'custom_expire_units'=>'nullable|string', //if expiration_rule_id is custom 
-            'annual_expire_month'=>'sometimes', //if expiration_rule_id is annual
-            'annual_expire_day'=> 'sometimes',  //if expiration_rule_id is annual integer
+            'custom_expire_offset'=>'required_if:expiration_rule_id,4|integer', //if expiration_rule_id is custom
+            'custom_expire_units'=>'required_if:expiration_rule_id,4|string', //if expiration_rule_id is custom 
+            'annual_expire_month'=>'required_if:expiration_rule_id,5|integer', //if expiration_rule_id is annual
+            'annual_expire_day'=> 'required_if:expiration_rule_id,5|integer',  //if expiration_rule_id is annual integer
             'date_begin'=> 'required|date_format:Y-m-d',
-            'date_end'=>'sometimes|date_format:Y-m-d',
-            'factor_before'=>'sometimes|numeric',
-            'factor_after'=>'sometimes|numeric',
+            'date_end'=>'required_if:expiration_rule_id,6|date_format:Y-m-d|after:date_begin', //if expiration_rule_id is specific date
+            'factor_before'=>'required_if:goal_plan_type_id,1|numeric',
+            'factor_after'=>'required_if:goal_plan_type_id,1|numeric',
             'is_recurring'=>'sometimes|boolean',
             'award_per_progress'=>'sometimes|boolean',
             'award_email_per_progress'=>'sometimes|boolean',
@@ -65,4 +65,24 @@ class GoalPlanRequest extends FormRequest
              'expired',
         ];
     }
+     /**
+          * Get the validation messages that apply to the request.
+          *
+          * @return array
+          */
+         /* public function messages()
+          {
+              return [
+                //'exceeded_event_id.required' =>"The exceeded event id field is required when goal plan type is 'Sales Goal'",
+                /*'oldpassword.required' => Lang::get('userpasschange.oldpasswordrequired'),
+                'oldpassword.max' => Lang::get('userpasschange.oldpasswordmax255'),
+                'newpassword.required' => Lang::get('userpasschange.newpasswordrequired'),
+                'newpassword.min' => Lang::get('userpasschange.newpasswordmin6'),
+                'newpassword.max' => Lang::get('userpasschange.newpasswordmax255'),
+                'newpassword.alpha_num' =>Lang::get('userpasschange.newpasswordalpha_num'),
+                'newpasswordagain.required' => Lang::get('userpasschange.newpasswordagainrequired'),
+                'newpasswordagain.same:newpassword' => Lang::get('userpasschange.newpasswordagainsamenewpassword'),
+                'username.max' => 'The :attribute field must  have under 255 chars',*/
+             // ];
+          //}
 }
