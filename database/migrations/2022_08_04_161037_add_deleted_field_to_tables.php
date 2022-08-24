4190713670d6f0ab4619a24cc342d6ace31c9092
Arvind Kumar <arvind@inimisttech.com>
2022-08-24 19:54:07 +0530

    fixing deleted issue in migration

--- database/migrations/2022_08_04_161037_add_deleted_field_to_tables.php
+++ database/migrations/2022_08_04_161037_add_deleted_field_to_tables.php
@@ -14,13 +14,19 @@ class AddDeletedFieldToTables extends Migration
     public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->timestamp('deleted_at')->nullable();
+            if(!$table->hasColumn('deleted_at'))  {
+                $table->timestamp('deleted_at')->nullable();
+            }
         });
         Schema::table('invoices', function (Blueprint $table) {
-            $table->timestamp('deleted_at')->nullable();
+            if(!$table->hasColumn('deleted_at'))  {
+                $table->timestamp('deleted_at')->nullable();
+            }
         });
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('deleted_at')->nullable();
+            if(!$table->hasColumn('deleted_at'))  {
+                $table->timestamp('deleted_at')->nullable();
+            }
         });
     }
 
@@ -31,14 +37,5 @@ class AddDeletedFieldToTables extends Migration
      */
     public function down()
     {
-        Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('deleted_at');
-        });        
-        Schema::table('invoices', function (Blueprint $table) {
-            $table->dropColumn('deleted_at');
-        });
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('deleted_at');
-        });
     }
 }
