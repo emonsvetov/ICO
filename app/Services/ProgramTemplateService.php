876a31dc699eadc49f5a18a0b9bebf0026387a3c
olaccursed <olaccursed@yandex.ru>
2022-12-28 14:48:17 +0300

    ICO-148. Moving theme the setting to admin - Done.

--- app/Services/ProgramTemplateService.php
+++ app/Services/ProgramTemplateService.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ProgramTemplateRequest;
 use App\Http\Traits\ProgramTemplateMediaUploadTrait;
 use App\Models\Program;
 use App\Models\ProgramTemplate;
+use Illuminate\Support\Facades\Storage;
 
 class ProgramTemplateService
 {
@@ -27,7 +28,7 @@ class ProgramTemplateService
         if ($uploads) {
             $newProgramTemplate->update($uploads);
         }
-        
+
         return $newProgramTemplate;
     }
 
@@ -39,24 +40,44 @@ class ProgramTemplateService
      */
     public function update(ProgramTemplateRequest $request, ProgramTemplate $programTemplate, Program $program): ?ProgramTemplate
     {
-        $validated = $request->validated();
-        $fieldsToUpdate = [
-            'welcome_message' => $validated['welcome_message'],
-            'button_color' => $validated['button_color'],
-            'button_bg_color' => $validated['button_bg_color'],
-            'button_corner' => $validated['button_corner'],
-            'font_family' => $validated['font_family'],
-        ];
-
-        $uploads = $this->handleProgramTemplateMediaUpload($request, $program);
-
-        if ($uploads) {
-            foreach ($uploads as $key => $upload) {
+        $resetTheme = $request->get('resetTheme');
+        if ($resetTheme != 'false'){
+            foreach($programTemplate->getAttributes() as $key=> $item){
+                if (!in_array($key, ['id', 'name', 'created_at', 'updated_at', 'program_id', 'is_active', 'update_id','updated_by'])){
+                    $fieldsToUpdate[$key] = null;
+                }
+                if ($key == 'button_corner'){
+                    $fieldsToUpdate[$key] = 0;
+                }
                 if (in_array($key, ProgramTemplate::IMAGE_FIELDS)) {
-                    $fieldsToUpdate[$key] = $upload;
+                    $oldFile = $programTemplate[$key];
+                    if( $oldFile )  {
+                        Storage::delete( $oldFile );
+                    }
+                }
+            }
+        } else {
+            $validated = $request->validated();
+            $fieldsToUpdate = [
+                'welcome_message' => $validated['welcome_message'] == 'null' ? null : $validated['welcome_message'],
+                'button_color' => $validated['button_color'],
+                'button_bg_color' => $validated['button_bg_color'],
+                'button_corner' => $validated['button_corner'],
+                'font_family' => $validated['font_family'] == 'null' ? null : $validated['font_family'],
+                'name' => $validated['name'],
+            ];
+
+            $uploads = $this->handleProgramTemplateMediaUpload($request, $program, $programTemplate, true);
+
+            if ($uploads) {
+                foreach ($uploads as $key => $upload) {
+                    if (in_array($key, ProgramTemplate::IMAGE_FIELDS)) {
+                        $fieldsToUpdate[$key] = $upload;
+                    }
                 }
             }
         }
+
         $programTemplate->update($fieldsToUpdate);
 
         return $programTemplate;
