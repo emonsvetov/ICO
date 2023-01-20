1ac669973ea8c626f260fd21c961eff2a352aef3
Amol Kaur <amol@inimisttech.com>
2023-01-20 18:14:06 +0530

    team section

--- routes/api.php
+++ routes/api.php
@@ -387,17 +387,16 @@ Route::middleware(['auth:api', 'json.response', 'verified'])->group(function () 
     Route::get('/v1/country/{country}/state',[App\Http\Controllers\API\CountryController::class, 'listStates']);
 
     //Team Routes
-     //Domain Routes
-     Route::get('/v1/organization/{organization}/team',
-     [App\Http\Controllers\API\TeamController::class, 'index'])->name('api.v1.team.index')->middleware('can:viewAny,App\Team,organization');
-     Route::post('/v1/organization/{organization}/team',
-     [App\Http\Controllers\API\TeamController::class, 'store'])->name('api.v1.team.store')->middleware('can:create,App\Team,organization');
-     Route::get('/v1/organization/{organization}/team/{team}',
-     [App\Http\Controllers\API\TeamController::class, 'show'])->name('api.v1.team.show')->middleware('can:view,App\Team,organization,team');
-     Route::put('/v1/organization/{organization}/team/{team}',
-     [App\Http\Controllers\API\TeamController::class, 'update'])->name('api.v1.team.update')->middleware('can:update,App\Team,organization,team');
-     Route::delete('/v1/organization/{organization}/team/{team}',
-     [App\Http\Controllers\API\TeamController::class, 'delete'])->name('api.v1.team.delete')->middleware('can:delete,App\Team,organization,team');
+     Route::get('/v1/organization/{organization}/program/{program}/team',
+     [App\Http\Controllers\API\TeamController::class, 'index'])->name('api.v1.team.index')->middleware('can:viewAny,App\Team,organization,program');
+     Route::post('/v1/organization/{organization}/program/{program}/team',
+     [App\Http\Controllers\API\TeamController::class, 'store'])->middleware('can:create,App\Team,organization,program');
+     Route::get('/v1/organization/{organization}/program/{program}/team/{team}',
+     [App\Http\Controllers\API\TeamController::class, 'show'])->name('api.v1.team.show')->middleware('can:view,App\Team,organization,program,team');
+     Route::put('/v1/organization/{organization}/program/{program}/team/{team}',
+     [App\Http\Controllers\API\TeamController::class, 'update'])->name('api.v1.team.update')->middleware('can:update,App\Team,organization,program,team');
+     Route::delete('/v1/organization/{organization}/program/{program}/team/{team}',
+     [App\Http\Controllers\API\TeamController::class, 'delete'])->name('api.v1.team.delete')->middleware('can:delete,App\Team,organization,program,team');
 
     Route::group([
         'prefix' => '/v1/organization/{organization}/program/{program}',
