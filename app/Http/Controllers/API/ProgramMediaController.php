b7c7255a4ea465cedba86bda0b28caf3a7c5320e
Evgeny Monsvetov <emonsvetov@incento.com>
2023-02-13 10:47:23 -0600

    ICO-V3-97. Add ability to upload brochures, newsletters, videos and more - round 3.

--- app/Http/Controllers/API/ProgramMediaController.php
+++ app/Http/Controllers/API/ProgramMediaController.php
@@ -74,35 +74,6 @@ class ProgramMediaController extends Controller
         return response()->json($responseFiles);
     }
 
-    public function show( Organization $organization, Program $program )
-    {
-
-        if ( $program )
-        {
-            if( !request()->get('only') ){
-                $program->load(['domains', 'merchants', 'organization', 'address', 'status']);
-            }
-            // $program->getTemplate();
-            return response( $program );
-        }
-
-        return response( [] );
-    }
-
-    public function update(ProgramRequest $request, Organization $organization, Program $program, ProgramService $programService )
-    {
-        $program = $programService->update( $program, $request->validated());
-        return response([ 'program' => $program ]);
-    }
-
-    public function move(ProgramMoveRequest $request, Organization $organization, Program $program )
-    {
-        // return $request->all();
-        // return $request->validated();
-        $program->update( $request->validated() );
-        return response([ 'program' => $program ]);
-    }
-
     public function delete(Organization $organization, Program $program )
     {
         $program->delete();
@@ -110,35 +81,4 @@ class ProgramMediaController extends Controller
         return response([ 'delete' => true ]);
     }
 
-    public function restore(Organization $organization, Program $program )
-    {
-        $program->restore();
-        $program->update(['status'=>'active']);
-        return response([ 'success' => true ]);
-    }
-
-    public function getPayments(Organization $organization, Program $program, ProgramPaymentService $programPaymentService)  {
-        $payments = $programPaymentService->getPayments($program);
-        return response($payments);
-    }
-
-    public function submitPayments(ProgramPaymentRequest $request, Organization $organization, Program $program, ProgramPaymentService $programPaymentService)  {
-        $result = $programPaymentService->submitPayments($program, $request->validated());
-        return response($result);
-    }
-
-    public function reversePayment(ProgramPaymentReverseRequest $request, Organization $organization, Program $program, Invoice $invoice, ProgramPaymentService $programPaymentService)  {
-        $result = $programPaymentService->reversePayment($program, $invoice, $request->validated());
-        return response($result);
-    }
-
-    public function getTransferMonies(Organization $organization, Program $program, ProgramService $programService)  {
-        $result = $programService->getTransferMonies($program);
-        return response($result);
-    }
-
-    public function submitTransferMonies(ProgramTransferMoniesRequest $request, Organization $organization, Program $program, ProgramService $programService)  {
-        $result = $programService->submitTransferMonies($program, $request->validated());
-        return response($result);
-    }
 }
