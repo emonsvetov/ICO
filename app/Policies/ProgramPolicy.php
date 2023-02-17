f8c3ae07e44d15c2f63d0ef5afcff3462ebedf74
Evgeny Monsvetov <emonsvetov@incento.com>
2023-02-17 16:20:19 -0600

    ICO-V3-97. Add ability to upload brochures, newsletters, videos and more - done.

--- app/Policies/ProgramPolicy.php
+++ app/Policies/ProgramPolicy.php
@@ -29,11 +29,7 @@ class ProgramPolicy
     public function viewAny(User $user, Organization $organization)
     {
         if( !$this->__preAuthCheck($user,$organization) ) return false;
-        if( $user->isAdmin() )
-        {
-            return true;
-        }
-        return $user->can('program-list');
+        return true;
     }
 
     public function view(User $user, Organization $organization, Program $program)
@@ -66,61 +62,5 @@ class ProgramPolicy
         if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
         if( $user->isAdmin() ) return true;
         return $user->can('program-delete');
-    }
-
-    public function move(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-move');
-    }
-
-    public function restore(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-restore');
-    }
-
-    public function listPayments(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-list-payments');
-    }
-
-    public function updatePayments(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-update-payments');
-    }
-
-    public function reversePayments(User $user, Organization $organization, Program $program, Invoice $invoice)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program, $invoice) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-reverse-payments');
-    }
-
-    public function transferMonies(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-transfer-monies');
-    }    
-    
-    public function listStatus(User $user, Organization $organization)
-    {
-        if( !$this->__preAuthCheck($user, $organization) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-list-status');
-    }
-
-    public function updateStatus(User $user, Organization $organization, Program $program)
-    {
-        if( !$this->__preAuthCheck($user, $organization, $program) ) return false;
-        if( $user->isAdmin() ) return true;
-        return $user->can('program-update-status');
     }
 }
