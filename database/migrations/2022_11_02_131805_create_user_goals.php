a8aa7e9888448cfa5190e688fe2afb5dbb6665d9
Amol Kaur <amol@inimisttech.com>
2023-03-01 21:02:55 +0530

    tabl exists check

--- database/migrations/2022_11_02_131805_create_user_goals.php
+++ database/migrations/2022_11_02_131805_create_user_goals.php
@@ -13,27 +13,29 @@ class CreateUserGoals extends Migration
      */
     public function up()
     {
-        Schema::create('user_goals', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
-            $table->integer('goal_plan_id');
-            $table->integer('next_user_goal_id')->nullable();
-            $table->integer('previous_user_goal_id')->nullable();
-            $table->decimal('target_value',11,4);
-            $table->integer('achieved_callback_id')->nullable();
-            $table->integer('exceeded_callback_id')->nullable();
-            $table->timestamp('date_met')->nullable();
-            $table->timestamp('date_exceeded')->nullable();
-            $table->decimal('factor_before', 9,4)->nullable(); //only for sales goal plan type
-            $table->decimal('factor_after', 9,4)->nullable();  //only for sales goal plan type
-            $table->float('calc_progress_total')->nullable();
-            $table->float('calc_progress_percentage')->nullable();
-            $table->integer('created_by');
-            $table->integer('modified_by')->nullable();
-            $table->timestamp('expired')->nullable();
-            $table->timestamp('deleted')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('user_goals')) {
+            Schema::create('user_goals', function (Blueprint $table) {
+                $table->id();
+                $table->integer('user_id');
+                $table->integer('goal_plan_id');
+                $table->integer('next_user_goal_id')->nullable();
+                $table->integer('previous_user_goal_id')->nullable();
+                $table->decimal('target_value',11,4);
+                $table->integer('achieved_callback_id')->nullable();
+                $table->integer('exceeded_callback_id')->nullable();
+                $table->timestamp('date_met')->nullable();
+                $table->timestamp('date_exceeded')->nullable();
+                $table->decimal('factor_before', 9,4)->nullable(); //only for sales goal plan type
+                $table->decimal('factor_after', 9,4)->nullable();  //only for sales goal plan type
+                $table->float('calc_progress_total')->nullable();
+                $table->float('calc_progress_percentage')->nullable();
+                $table->integer('created_by');
+                $table->integer('modified_by')->nullable();
+                $table->timestamp('expired')->nullable();
+                $table->timestamp('deleted')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
