db42426cc95995f90cb817a156c520324ad0f43a
Evgeny Monsvetov <emonsvetov@incento.com>
2023-03-14 01:06:58 +0300

    ICO-1631. Connect V2 and V3 - API changes 2.

--- app/Http/Controllers/API/CheckoutController.php
+++ app/Http/Controllers/API/CheckoutController.php
@@ -4,23 +4,44 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Requests\CheckoutRequest;
 use App\Http\Controllers\Controller;
+use App\Models\Giftcode;
 use App\Services\CheckoutService;
 use App\Models\Organization;
 use App\Models\Program;
 use App\Models\Country;
 use App\Models\State;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class CheckoutController extends Controller
 {
     public function store(CheckoutRequest $request, CheckoutService $checkoutService, Organization $organization, Program $program )
     {
-
         $cart = $request->validated();
         try {
             $response = $checkoutService->processOrder( $cart, $program );
             if( !empty($response['errors']) )   {
                 return response(['errors' => $response['errors']], 422);
             }
+
+            // alert V2
+            if (isset($response['gift_codes_redeemed_for'])) {
+                $giftCodes = $response['gift_codes_redeemed_for'];
+                foreach ($giftCodes as $codeItem) {
+                    $giftCodeId = (int)$codeItem->id;
+                    $giftCode = Giftcode::find($giftCodeId);
+                    $responseV2 = Http::withHeaders([
+                        'X-API-KEY' => 'f0b74774b92165716b1e08b3667ddcaf0a7511bb',
+                    ])->post(env('V2_API') . '/rest/gift_codes/redeem', [
+                        'v3_merchant_id' => $giftCode->code,
+                    ]);
+//                    Log::info('V2: ' . $giftCode->code);
+                    if ($responseV2->status() !== 200 ){
+                        Log::debug($responseV2->body());
+                    }
+                }
+            }
+
             return $response;
         }   catch (\Exception $e)    {
             return response(
