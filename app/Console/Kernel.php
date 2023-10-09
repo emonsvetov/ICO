8ae3fe25407ef2e4dab68d5d5b2d03e13574a6dd
Production Server <ubuntu@ip-172-31-30-199.ec2.internal>
2023-10-09 13:29:27 +0000

    enabling award cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('cron:monthly-invoicing')->monthly();
         $schedule->command('cron:submit-tango-orders')->hourly();
         $schedule->command('cron:generate-virtual-inventory')->everyFiveMinutes();
-        //$schedule->command('cron:send-milestone-award')->daily();
+        $schedule->command('cron:send-milestone-award')->daily();
     }
 
     /**
