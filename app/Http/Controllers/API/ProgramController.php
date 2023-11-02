66e1128b330b976c160bc6575d7f2a6b1a9ce810
Production Server <ubuntu@ip-172-31-30-199.ec2.internal>
2023-11-02 02:05:54 +0000

    adding two files. some unknown change on live

--- app/Http/Controllers/API/ProgramController.php
+++ app/Http/Controllers/API/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Mail\templates\WelcomeEmail;
 use Illuminate\Support\Facades\DB;
 
 use App\Http\Requests\ProgramPaymentReverseRequest;
@@ -28,6 +29,7 @@ use App\Models\Posting;
 use App\Models\Invoice;
 use App\Models\Event;
 use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class ProgramController extends Controller
 {
