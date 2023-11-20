96f6ae0bcef753cfb7ceccbaf4d978788215ef02
crypopanda430 <panda.kk1114@gmail.com>
2023-11-20 05:10:06 -0300

    fix journal-detail report

--- app/Services/reports/ReportJournalDetailedService.php
+++ app/Services/reports/ReportJournalDetailedService.php
@@ -437,6 +437,12 @@ class ReportJournalDetailedService extends ReportServiceAbstract
         foreach( $this->table as $i => $program) {
             $this->table[$i]->net_points_purchased = $this->table[$i]->points_purchased - $this->table[$i]->reclaims - $this->table[$i]->award_credit_reclaims;
         }
+
+		$temp = array();
+		foreach( $this->table as $i => $program) {
+			array_push($temp, $program);
+		}
+		$this->table = $temp;
 	}
 
 	/** Calculate data by date range (timestampFrom|To) */
