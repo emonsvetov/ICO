5c593aa537472814bcf00d49ba5bc53de0dab014
Oleg <olegganshonkov@gmail.com>
2024-01-15 16:59:27 +0300

    ICOV3-177. Invite participant (Manager side) - Done.

--- app/Http/Controllers/API/InvitationController.php
+++ app/Http/Controllers/API/InvitationController.php
@@ -27,56 +27,52 @@ class InvitationController extends Controller
     public function invite(InvitationRequest $request, Organization $organization, Program $program)
     {
         DB::beginTransaction();
-		try {
+        try {
             $validated = $request->validated();
             $validated['organization_id'] = $organization->id;
             $generatedPassword = rand();
             $validated['password'] = $generatedPassword;
 
-            $user = User::createAccount( $validated );
+            $user = User::createAccount($validated);
             // $user = User::find( 553 );
             $token = Password::broker()->createToken($user);
 
             $roles[] = Role::getIdByName(config('roles.participant'));
 
-            if( !empty($roles) )
-            {
-                $program->users()->sync( [ $user->id ], false );
+            if (!empty($roles)) {
+                $program->users()->sync([$user->id], false);
                 $user->syncProgramRoles($program->id, $roles);
             }
-            // event(new UserInvited($user, $program, $token));
+            event(new UserInvited($user, $program, $token));
             DB::commit();
-            return response([ 'user' => $user ]);
-        } catch (\Exception $e )    {
+            return response(['user' => $user]);
+        } catch (\Exception $e) {
             DB::rollBack();
-            if( config('app.env') != 'production')
-            {
+            if (config('app.env') != 'production') {
                 $error = sprintf('Error inviting user (%d) to program (%d). Exception "%s" on line %d in file %s ', $user->id, $program->id, $e->getMessage(), $e->getLine(), $e->getFile());
-            }
-            else
-            {
+            } else {
                 $error = "Error inviting user to program";
             }
             return response(['errors' => $error], 422);
         }
-	}
+    }
+
     public function resend(InvitationResendRequest $request, Organization $organization, Program $program)
     {
         //return auth()->user();
-		try {
+        try {
             $validated = $request->validated();
             $recipients = $validated['recipients'];
             $users = User::find($recipients);
-            foreach($users as $user)
-            {
+            foreach ($users as $user) {
                 $user->token = Password::broker()->createToken($user);
             }
-            event( new UsersInvited( $users, $program, true ) );
-            return response([ 'success' => true ]);
-        } catch (\Exception $e )    {
+            event(new UsersInvited($users, $program, true));
+            return response(['success' => true]);
+        } catch (\Exception $e) {
             return response(['errors' => $e->getMessage()], 422);
         }
-	}
+    }
 
     public function accept(Request $request, DomainService $domainService)
     {
@@ -86,8 +82,7 @@ class InvitationController extends Controller
             'password' => 'required|confirmed'
         ]);
 
-        if( $domainService->isValidDomain() )
-        {
+        if ($domainService->isValidDomain()) {
             $response = [];
             $status = Password::reset(
                 $request->only('email', 'password', 'password_confirmation', 'token'),
@@ -98,7 +93,7 @@ class InvitationController extends Controller
                         'email_verified_at' => now(),
                         'user_status_id' => User::getIdStatusActive()
                     ])->save();
-    
+
                     $user->tokens()->delete();
                     event(new InvitationAccepted($user));
                 }
@@ -106,23 +101,19 @@ class InvitationController extends Controller
 
             if ($status == Password::PASSWORD_RESET) {
                 $response = [
-                    'message'=> 'Invitation accepted successfully'
+                    'message' => 'Invitation accepted successfully'
                 ];
-                if(auth()->guard('web')->attempt( ['email' => $request->email, 'password' => $request->password] ))
-                {
+                if (auth()->guard('web')->attempt(['email' => $request->email, 'password' => $request->password])) {
                     $user = auth()->guard('web')->user();
                     $domain = $domainService->getDomain();
 
-                    if($user && $domain)
-                    {
+                    if ($user && $domain) {
                         $user->load(['organization']);
-                        $user->programRoles = $user->getCompiledProgramRoles(null, $domain );
-                        if( $user->programRoles )  {
-                            foreach( $user->programRoles as $programRole)
-                            {
+                        $user->programRoles = $user->getCompiledProgramRoles(null, $domain);
+                        if ($user->programRoles) {
+                            foreach ($user->programRoles as $programRole) {
                                 $program = Program::find($programRole['id']);
-                                if( $program->exists() )
-                                {
+                                if ($program->exists()) {
                                     $accessToken = auth()->guard('web')->user()->createToken('authToken')->accessToken;
                                     $response['access_token'] = $accessToken;
                                     $response['user'] = $user;
@@ -139,7 +130,7 @@ class InvitationController extends Controller
         }
 
         return response([
-            'message'=> __($status)
+            'message' => __($status)
         ], 500);
     }
 }
