839bc7f6ff70ec51d70e01d31dcb00894d140b80
andreamelkov <andreamelkov61@gmail.com>
2024-02-22 14:46:53 +0300

    ICOV3-203. migrate domains, updating exists

--- app/Services/v2migrate/MigrateDomainsService.php
+++ app/Services/v2migrate/MigrateDomainsService.php
@@ -9,7 +9,8 @@ class MigrateDomainsService extends MigrationService
     // ?
     const V3_ORGANIZATION_ID = 1;
 
-    public $countNewDomains = 0;
+    public $countCreatedDomains = 0;
+    public $countUpdatedDomains = 0;
 
     public function __construct()
     {
@@ -52,7 +53,7 @@ class MigrateDomainsService extends MigrationService
 
             return [
                 'success' => TRUE,
-                'info' => "was migrated $this->countNewDomains items",
+                'info' => "created $this->countCreatedDomains items, updated $this->countUpdatedDomains items",
             ];
         } catch(Exception $e) {
             throw new Exception("Error migrating domains. Error:{$e->getMessage()} in Line: {$e->getLine()} in File: {$e->getFile()}");
@@ -65,38 +66,39 @@ class MigrateDomainsService extends MigrationService
      */
     public function migrateDomain($v2Domain)
     {
-        $v3DomainByID = Domain::where('v2_domain_id', $v2Domain->access_key)->first();
+        $v3Domain = Domain::where('v2_domain_id', $v2Domain->access_key)->first();
         $v3DomainByName = Domain::where('name', $v2Domain->name)->first();
 
-        if (!blank($v3DomainByName) && blank($v3DomainByID)) {
+        if (!blank($v3DomainByName) && blank($v3Domain)) {
             throw new Exception("Error migrating domains. Domain $v3DomainByName->name is exists and column v2_domain_id is empty from v3.");
         }
 
-        if (blank($v3DomainByID)) {
+        $v3DomainData = [
+            'organization_id' => self::V3_ORGANIZATION_ID,
+            'name' => $v2Domain->name,
+            'secret_key' => $v2Domain->secret_key,
+            'v2_domain_id' => $v2Domain->access_key
+        ];
 
-            $v3DomainIPs = [];
-            $v3DomainData = [
-                'organization_id' => self::V3_ORGANIZATION_ID,
-                'name' => $v2Domain->name,
-                'secret_key' => $v2Domain->secret_key,
-                'v2_domain_id' => $v2Domain->access_key
-            ];
-
+        if (blank($v3Domain)) {
             $v3Domain = Domain::create($v3DomainData);
-            $this->countNewDomains++;
-            $v2DomainIPs =  $this->v2db->select("SELECT * FROM `domains_ips` WHERE domain_access_key = {$v2Domain->access_key}");
-            if (!blank($v2DomainIPs)) {
-                foreach ($v2DomainIPs as $v2DomainIP) {
-                    $v3DomainIPs[] = [
-                        'domain_id' => $v3Domain->id,
-                        'ip_address' => $v2DomainIP->ip_address,
-                    ];
-                }
-                $v3Domain->domain_ips()->createMany($v3DomainIPs);
-            }
+            $this->countCreatedDomains++;
+        }
+        else {
+            $v3Domain->update($v3DomainData);
+            $this->countUpdatedDomains++;
+        }
 
-            // TODO create v2_domain_id in domains v2, update the column?
+        $v2DomainIPs =  $this->v2db->select("SELECT * FROM `domains_ips` WHERE domain_access_key = {$v2Domain->access_key}");
+        $v3Domain->domain_ips()->delete();
+        if (!blank($v2DomainIPs)) {
+            foreach ($v2DomainIPs as $v2DomainIP) {
+                $v3DomainIPs[] = [
+                    'domain_id' => $v3Domain->id,
+                    'ip_address' => $v2DomainIP->ip_address,
+                ];
+            }
+            $v3Domain->domain_ips()->createMany($v3DomainIPs);
         }
     }
-
 }
