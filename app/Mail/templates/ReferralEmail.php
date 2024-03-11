52e342c44accdc2c390d7d2aa48c30403122ce8c
cryptofan430 <panda.kk1114@gmail.com>
2024-03-11 13:43:02 -0400

    Add files for referral email templates

---
+++ app/Mail/templates/ReferralEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail\templates;
+
+use App\Models\EmailTemplate;
+use App\Mail\SendgridEmail;
+
+class ReferralEmail extends SendgridEmail
+{
+    public string $type = 'emails.referral';
+    public $subject = 'You have a new referral';
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        string $contactFirstName,
+        string $referrer_first_name,
+        string $referrer_last_name,
+        string $referee_first_name,
+        string $referee_last_name,
+        string $referee_email,
+        string $referee_area_code,
+        string $referee_phone,
+        $program
+    )
+    {
+        parent::__construct();
+        $this->init(func_get_args());
+    }
+}
