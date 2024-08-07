f308508e14c4e494ec18ddb994820298b3d5253d
Ashwani Kumar <ashwanikumar@inimisttech.com>
2024-08-07 09:44:01 +0530

    fixed parent id program

--- app/Http/Controllers/API/BudgetProgramController.php
+++ app/Http/Controllers/API/BudgetProgramController.php
@@ -37,8 +37,8 @@ class BudgetProgramController extends Controller
 
     public function index(Organization $organization, Program $program)
     {
-        $program = new Program();
-        $program_id = $program->get_top_level_program_id($program->id);
+        $p_program = new Program();
+        $program_id = $p_program->get_top_level_program_id($program->id);
         $budgetPrograms = BudgetProgram::where('program_id', $program_id)
             ->with('budget_types')
             ->get();
@@ -98,8 +98,8 @@ class BudgetProgramController extends Controller
     public function getBudgetCascadingApproval(Organization $organization, Program $program, string $title, Request $request)
     {
         if ($title) {
-            $program = new Program();
-            $program_id = $program->get_top_level_program_id($program->id);
+            $p_program = new Program();
+            $program_id = $p_program->get_top_level_program_id($program->id);
             $approved = $title == 'manage-approvals' ? 1 : ($title == "cascading-approvals" ? 0 : response([]));
             self::$query = BudgetCascadingApproval::where('program_id', $program_id)
                 ->where('approved', $approved)
